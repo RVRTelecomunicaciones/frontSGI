1c21f5dd5915cf3448316051ff8cf6b85dfaba03
Russell Franz Vergara Rojas <sisalm0202@gmail.com>
2021-11-19 17:29:46 -0500

    Login form

--- src/app/core/pages/login/login.component.ts
+++ src/app/core/pages/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,24 +8,51 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  validateForm!: FormGroup;
+  formLogin!: FormGroup;
 
-  submitForm(): void {
-    for (const i in this.validateForm.controls) {
-      if (this.validateForm.controls.hasOwnProperty(i)) {
-        this.validateForm.controls[i].markAsDirty();
-        this.validateForm.controls[i].updateValueAndValidity();
-      }
+  isFormSubmitted = false;
+
+  @ViewChild('focusInitial')
+  focusInitial!: ElementRef<HTMLInputElement>;
+
+  async submitLogin() {
+    // Return if form is invalid
+    if (this.formLogin.invalid) {
+      return;
+    }
+    // Set flag to true
+    this.isFormSubmitted = true;
+  }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    // Patterns
+    const PAT_EMAIL = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+[.][a-zA-Z]{2,4}$';
+
+    this.formLogin = this.formBuilder.group({
+      usernameOrEmail: [
+        '',
+        [Validators.required, Validators.pattern(PAT_EMAIL)],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+
+    if (this.focusInitial !== undefined) {
+      this.focusInitial.nativeElement.focus();
+      console.log('focus');
     }
   }
 
-  constructor(private fb: FormBuilder) {}
+  ngAfterViewInit(): void {
+    requestAnimationFrame(() => this.focusInitial.nativeElement.focus());
+  }
 
-  ngOnInit(): void {
-    this.validateForm = this.fb.group({
-      userName: [null, [Validators.required]],
-      password: [null, [Validators.required]],
-      remember: [true],
-    });
+  get f() {
+    return this.formLogin.controls;
   }
 }
